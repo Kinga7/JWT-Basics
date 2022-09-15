7d09f76bf6c41d1a4f6c67056bc4107d83cd391a
Austin King <awkscbw@gmail.com>
2022-09-15 07:42:51 -0700

    setting up the JWT

--- controllers/main.js
+++ controllers/main.js
@@ -1,10 +1,38 @@
-
-
+const jwt = require(`jsonwebtoken`)
+const CustomAPIError = require(`../errors/custom-error`)
 const login = async (req, res) => {
-    res.send(`fake login/Register/Signup Route`)
+    const {username,password} = req.body
+    // Mongo 
+    // Joi
+    // Check in the controller
+
+    if(!username || !password){
+        throw new CustomAPIError(`please provide email and password`, 400)
+    }
+
+    // Just for demo, normally provided by DB!!!
+    const id = new Date().getDate()
+
+    // Try to keep payload small, better experience for user
+    const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:`30d`})
+
+    res.status(200).json({msg:`user created`, token})
 }
 
 const dashboard = async (req, res) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith(`Bearer `))
+    throw new CustomAPIError(`No token provided`,401)
+
+    const token = authHeader.split(` `)[1]
+
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+    } catch (error) {
+        throw new CustomAPIError(`Not authorized to access this route`,401)
+    }
+
+
     const luckyNumber = Math.floor(Math.random()*100)
     res.status(200).json({msg:`Hello, John Doe`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
 }
